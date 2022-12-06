ffe233d091a0dde2ac401b3fe3cb26212ed7ede0
akapoorcern <akapoor@cern.ch>
2022-12-06 04:54:13 +0100

    new scale uncertainty json files

--- JSONTools.py
+++ JSONTools.py
@@ -89,7 +89,7 @@ def SFyearwise(files=[],names=[],valtypes=["sf","sfup","sfdown"]):
     })
     return output
 
-def getSS(year,val,etabin,gain):
+def getScaleUnc(year,val,etabin,gain):
     
     SandSdict={
         "2016preVFP" :{ "EBin":{12 : 0.05, 6 : 0.5, 1 : 2.0}, "EBout":{12 : 0.075, 6 : 0.5, 1 : 2.0}, "EE":{12 : 0.15,  6 : 1.0,  1 : 3.0} },
@@ -98,15 +98,23 @@ def getSS(year,val,etabin,gain):
         "2018"        :{ "EBin":{12 : 0.05, 6 : 0.5, 1 : 1.0}, "EBout":{12 : 0.05,  6 : 0.5, 1 : 1.0}, "EE":{12 : 0.125, 6 : 0.75, 1 : 2.0} }
     }
     
-    if val=="ssup":
+    if val=="scaleup":
         return (1+ SandSdict[year][etabin][gain]/100.)
-    if val=="ssdown": 
+    elif val=="scaledown": 
         return (1- SandSdict[year][etabin][gain]/100.)
-    
+    else:
+        return (SandSdict[year][etabin][gain]/100.)
 
-def SS(year,valtypes=["ssup","ssdown"]):
-    
-    etabins=["EBin","EBout","EE"]
+def getScaleUncbins(year,val,gain):
+    etabins=[float('-inf'),-1.442, -1, 0, 1, 1.442, float('inf')]
+    return schema.MultiBinning.parse_obj({
+        "inputs":["eta"],
+        "nodetype": "multibinning",
+        "edges": [etabins],
+        "content": [getScaleUnc(year,val,etabin,gain) for etabin in ['EE','EBout','EBin','EBin','EBout','EE']] ,
+        "flow": 'error'})
+
+def ScaleUnc(year,valtypes=["scaleup","scaledown","scaleunc"]):
     gains=[12,6,1]
     output = schema.Category.parse_obj({
                 "nodetype": "category",
@@ -116,29 +124,18 @@ def SS(year,valtypes=["ssup","ssdown"]):
                         "key": val, 
                         "value":schema.Category.parse_obj({
                             "nodetype": "category",
-                            "input": "etabin",
-                            "content":[
-                                schema.CategoryItem.parse_obj({
-                                    "key": etabin, 
-                                    "value": schema.Category.parse_obj({
-                                        "nodetype": "category",
-                                        "input": "gain",
-                                        "content":[
-                                            schema.CategoryItem.parse_obj({
-                                                "key": gain, 
-                                                "value": getSS(year,val,etabin,gain)})
-                                            for gain in gains
-                                        ],
+                            "input": "gain",
+                            "content":[schema.CategoryItem.parse_obj({"key": gain, 
+                                                                      "value": getScaleUncbins(year,val,gain)})
+                                       for gain in gains
+                                      ],
                                     })
-                                })
-                                for etabin in etabins
-                            ],
-                        })
                     })
                     for val in valtypes
                 ],
     })
     return output
+
 
 
 def CSEVSFs(files,name,i,IsSF="sf"):
